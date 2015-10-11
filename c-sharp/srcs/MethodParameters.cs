f8e59851226f92ef71ed59aad3b5402e0828954b
Thomas Maurin <maurin.th@gmail.com>
2015-10-11 12:35:36 +0200

    completed MethodParameters

--- c-sharp/srcs/MethodParameters.cs
+++ c-sharp/srcs/MethodParameters.cs
@@ -8,10 +8,19 @@ class MethodParameters
 		SimpleMethod(ref i);
 		Console.WriteLine(i);
 
-		int Total = 0;
-		int Product = 0;
-		Calculate(10, 20, out Total, out Product);
-		Console.WriteLine("Sum = {0}, Product = {1}", Total, Product);
+		int Sum;
+		int Product;
+		Calculate(10, 20, out Sum, out Product);
+		Console.WriteLine("Sum = {0}, Product = {1}", Sum, Product);
+
+		var Numbers = new int[3];
+		Numbers[0] = 101;
+		Numbers[1] = 102;
+		Numbers[2] = 103;
+
+		ParamsMethod(Numbers);
+		ParamsMethod(1, 2, 3, 4, 5);
+		ParamsMethod();
 	}
 
 	public static void SimpleMethod(ref int j)
@@ -19,9 +28,18 @@ class MethodParameters
 		j = 101;
 	}
 
-	public static void Calculate(int x, int y, out int Sum, out int Product)
+	public static void Calculate(int x, int y, out int sum, out int product)
 	{
-		Sum = x + y;
-		Product = x * y;
+		sum = x + y;
+		product = x * y;
+	}
+
+	public static void ParamsMethod(params int[] numbers)
+	{
+		Console.WriteLine("There are {0} elements", numbers.Length);
+		foreach(int i in numbers)
+		{
+			Console.WriteLine(i);
+		}
 	}
 }

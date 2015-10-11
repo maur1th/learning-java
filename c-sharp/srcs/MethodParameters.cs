e1d063039d9804d74d67c560966a3b760bc093f5
Thomas Maurin <maurin.th@gmail.com>
2015-10-11 12:19:16 +0200

    MethodParameters stub

--- c-sharp/srcs/MethodParameters.cs
+++ c-sharp/srcs/MethodParameters.cs
@@ -7,10 +7,21 @@ class MethodParameters
 		int i = 0;
 		SimpleMethod(ref i);
 		Console.WriteLine(i);
+
+		int Total = 0;
+		int Product = 0;
+		Calculate(10, 20, out Total, out Product);
+		Console.WriteLine("Sum = {0}, Product = {1}", Total, Product);
 	}
 
 	public static void SimpleMethod(ref int j)
 	{
 		j = 101;
+	}
+
+	public static void Calculate(int x, int y, out int Sum, out int Product)
+	{
+		Sum = x + y;
+		Product = x * y;
 	}
 }

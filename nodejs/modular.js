fd93ff86627c93e06a6a3c69c51b47dcb64f23fc
Thomas Maurin <maurin.th@gmail.com>
2015-10-19 17:50:24 +0200

    modular

---
+++ nodejs/modular.js
@@ -0,0 +1,13 @@
+/*jslint node*/
+"use strict";
+var dir = process.argv[2];
+var ext = process.argv[3];
+var mymodule = require('./mymodule.js');
+mymodule(dir, ext, function (err, data) {
+    if (err) {
+        throw err;
+    }
+    data.forEach(function (file) {
+        console.log(file);
+    });
+});

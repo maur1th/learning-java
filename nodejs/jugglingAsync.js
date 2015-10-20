0eda681065814e824ae44bb8fd108cd7691efd3a
Thomas Maurin <maurin.th@gmail.com>
2015-10-20 12:00:55 +0200

    jugglingAsync

--- nodejs/jugglingAsync.js
+++ nodejs/jugglingAsync.js
@@ -1,2 +1,26 @@
 /*jslint es6, node*/
 "use strict";
+let http = require('http');
+let results = [];
+let cbCount = 0;
+process.argv.forEach(function (url, idx) {
+    if (idx < 2) {
+        return;
+    }
+    http.get(url, function (res) {
+        let tmp = "";
+        res.on('data', function (chunk) {
+            tmp += chunk;
+        });
+        res.on('end', function () {
+            cbCount += 1;
+            results[idx] = tmp.toString();
+            if(cbCount == 3) {
+                results.forEach(function (s) {
+                    console.log(s);
+                });
+            }
+        });
+        res.on('error', console.log);
+    });
+});

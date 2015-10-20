250ca5b81e29215081cdc07779f01b0aa57543a4
Thomas Maurin <maurin.th@gmail.com>
2015-10-20 14:32:02 +0200

    const where needed

--- nodejs/httpFileServer.js
+++ nodejs/httpFileServer.js
@@ -1,7 +1,7 @@
 /*jslint es6, node*/
 "use strict";
-let http = require('http');
-let fs = require('fs');
+const http = require('http');
+const fs = require('fs');
 http.createServer(function (ignore, res) {
     res.writeHead(200, {'content-type': 'text/plain'});
     fs.createReadStream(process.argv[3]).pipe(res);

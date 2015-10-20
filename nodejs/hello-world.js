de9f4c5f14fe6cab2164a9f191d81dc1c5927ae9
Thomas Maurin <maurin.th@gmail.com>
2015-10-20 09:45:39 +0200

    more explicit request

--- nodejs/hello-world.js
+++ nodejs/hello-world.js
@@ -1,8 +1,10 @@
 /*jslint node*/
 "use strict";
 var http = require('http');
-http.createServer(function (req, res) {
+var server = http.createServer();
+server.on('request', function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
-}).listen(3000);
+});
+server.listen(3000);
 console.log('Server running at http://localhost:3000/');

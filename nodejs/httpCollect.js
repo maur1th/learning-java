a42e50e90319934e763f901aef3dfe2b0c3a5ebd
Thomas Maurin <maurin.th@gmail.com>
2015-10-20 11:05:54 +0200

    added solution without using bl

--- nodejs/httpCollect.js
+++ nodejs/httpCollect.js
@@ -5,12 +5,24 @@ const bl = require('bl');
 
 http.get(process.argv[2], function (res) {
     res.setEncoding('utf-8');
+    // Using bl
     res.pipe(bl(function (err, data) {
         if (err) {
             throw err;
         }
+        console.log("## Using bl ##");
         data = data.toString();
         console.log(data.length);
         console.log(data);
     }));
+    // Without bl
+    var s = "";
+    res.on('data', function (chunk) {
+        s += chunk;
+    });
+    res.on('end', function () {
+        console.log("## Without bl ##");
+        console.log(s.length);
+        console.log(s);
+    })
 });

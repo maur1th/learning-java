5fcbcfdd5f51960af6d6d5fc4b24df4f238e30fd
Thomas Maurin <maurin.th@gmail.com>
2015-11-22 12:19:04 +0100

    removed java csharp stuff

--- java/HelloDate.java
+++
@@ -1,8 +0,0 @@
-// HelloDate.java
-import java.util.*;
-public class HelloDate {
-    public static void main(String[] args) {
-        System.out.println("Hello, it's: ");
-        System.out.println(new Date());
-    }
-}
